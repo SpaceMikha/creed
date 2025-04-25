1189da1120e1c9327190affa285dac9c70065d56
mikha <mikhaelmiro300@gmail.com>
2025-04-25 17:12:54 +0200

    add image for entity animation, direction for entity, counter for sprite anim, current sprite num for anim

--- game/src/Entity/Entity.java
+++ game/src/Entity/Entity.java
@@ -1,10 +1,16 @@
 package Entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
     
     public int x, y; // Position of the entity
     public int speed; // Speed of the entity
 
-    
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2; // Images for the entity's animations
+    public String direction; // Direction the entity is facing
+
+    public int spriteCounter = 0; // Counter for sprite animation
+    public int spriteNum = 1; // Current sprite number for animation
 
 }
