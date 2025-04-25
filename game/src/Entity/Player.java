3773910ed4499f4d8b7e47093bae0409d38d6082
mikha <mikhaelmiro300@gmail.com>
2025-04-25 17:14:50 +0200

    update movement logic, refactor player class to improve loading

--- game/src/Entity/Player.java
+++ game/src/Entity/Player.java
@@ -1,11 +1,11 @@
 package Entity;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
 import Main.GamePanel;
 import Main.KeyHandler;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Player extends Entity {
 
@@ -17,6 +17,7 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
@@ -24,27 +25,98 @@ public class Player extends Entity {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+
+    }
+
+    public void getPlayerImage() {
+
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right_2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void update() {
 
-        if (keyH.upPressed == true) {
-            y -= speed;
-        } else if (keyH.downPressed == true) {
-            y += speed;
-        } else if (keyH.leftPressed == true) {
-            x -= speed;
-        } else if (keyH.rightPressed == true) {
-            x += speed;
-        }
+        if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
+         
+            if (keyH.upPressed == true) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed == true) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed == true) {
+                direction = "left";
+                x -= speed;
+            } else if (keyH.rightPressed == true) {
+                direction = "right";
+                x += speed;
+            }
+    
+            // sprite counter for animation
+            spriteCounter++;
+            if(spriteCounter > 12){
+                if(spriteNum == 1) {
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
 
+        }
     }
 
     public void draw(Graphics2D g2) {
 
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
 
+        switch (direction) {
+            case "up":
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
+                break;
+            case "left":    
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
+                break;  
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
