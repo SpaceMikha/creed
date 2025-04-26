f9fafde106b7a1b2ac65c2755ecfdb97f38b1b55
mikha <mikhaelmiro300@gmail.com>
2025-04-27 00:03:18 +0200

    refactor GamePanel: improve code formatting and consistency

--- game/src/Main/GamePanel.java
+++ game/src/Main/GamePanel.java
@@ -10,91 +10,91 @@ import tiles.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
 
-	// SCREEN SETTINGS
-	final int originalTileSize = 16; // 16x16 tile
-	final int scale = 3;
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
 
-	public final int tileSize = originalTileSize * scale; // 48x48 tile
-	final int maxScreenCol = 16;
-	final int maxScreenRow = 12;
-	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
+    public int maxScreenCol = 16;
+    public int maxScreenRow = 12;
+    public int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
-	// FPS
-	int FPS = 60;
+    // FPS
+    int FPS = 60;
 
-	TileManager tileM = new TileManager(this);
-	KeyHandler keyH = new KeyHandler();
-	Thread gameThread;
-	Player player = new Player(this, keyH);
+    TileManager tileM = new TileManager(this);
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this, keyH);
 
-	// Game state
-	public GamePanel() {
+    // Game state
+    public GamePanel() {
 
-		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-		this.setBackground(Color.black);
-		this.setDoubleBuffered(true);
-		this.addKeyListener(keyH);
-		this.setFocusable(true);
-	}
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
 
-	public void startGameThread() {
+    public void startGameThread() {
 
-		gameThread = new Thread(this);
-		gameThread.start();
-	}
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		double drawInterval = 1000000000 / FPS; // 0.016666... seconds
-		double nextDrawTime = System.nanoTime() + drawInterval;
+        double drawInterval = 1000000000 / FPS; // 0.016666... seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
 
-		while (gameThread != null) {
+        while (gameThread != null) {
 
-			// update information such as character position
-			update();
+            // update information such as character position
+            update();
 
-			// draw the screen with the updated information
-			repaint();
+            // draw the screen with the updated information
+            repaint();
 
-			try {
+            try {
 
-				double remainingTime = nextDrawTime - System.nanoTime();
-				remainingTime = remainingTime / 1000000;
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000;
 
-				if (remainingTime < 0) {
-					remainingTime = 0;
-				}
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
 
-				Thread.sleep((long) remainingTime);
+                Thread.sleep((long) remainingTime);
 
-				nextDrawTime += drawInterval;
+                nextDrawTime += drawInterval;
 
-			} catch (InterruptedException e) {
+            } catch (InterruptedException e) {
 
-				e.printStackTrace();
-			}
-		}
+                e.printStackTrace();
+            }
+        }
 
-	}
+    }
 
-	public void update() {
+    public void update() {
 
-		player.update();
-	}
+        player.update();
+    }
 
-	public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
 
-		super.paintComponent(g);
+        super.paintComponent(g);
 
-		Graphics2D g2 = (Graphics2D) g;
+        Graphics2D g2 = (Graphics2D) g;
 
-		tileM.draw(g2);
-		
-		player.draw(g2);
+        tileM.draw(g2);
 
-		g2.dispose();
+        player.draw(g2);
 
-	}
+        g2.dispose();
+
+    }
 }
