47e5325ad38ca520a4850f16b0721099699b7482
mikha <mikhaelmiro300@gmail.com>
2025-04-26 15:14:19 +0200

    refactor GamePanel: fix import statement and organize tile manager initialization

--- game/src/Main/GamePanel.java
+++ game/src/Main/GamePanel.java
@@ -1,11 +1,12 @@
 package Main;
 
+import entity.Player;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JPanel;
-import Entity.Player;
+import tiles.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -22,15 +23,12 @@ public class GamePanel extends JPanel implements Runnable {
 	// FPS
 	int FPS = 60;
 
+	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	Player player = new Player(this, keyH);
 
-	// set player's default position
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
-
+	// Game state
 	public GamePanel() {
 
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -92,6 +90,8 @@ public class GamePanel extends JPanel implements Runnable {
 
 		Graphics2D g2 = (Graphics2D) g;
 
+		tileM.draw(g2);
+		
 		player.draw(g2);
 
 		g2.dispose();
