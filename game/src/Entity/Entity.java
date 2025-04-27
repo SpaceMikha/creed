68a63abf32c114f13853ac4c67cd97097d22c8f3
mikha <mikhaelmiro300@gmail.com>
2025-04-27 18:43:45 +0200

    refactor Entity: add solidArea and collisionOn variables for improved collision detection

--- game/src/Entity/Entity.java
+++ game/src/Entity/Entity.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Entity {
@@ -13,4 +14,6 @@ public class Entity {
     public int spriteCounter = 0; // Counter for sprite animation
     public int spriteNum = 1; // Current sprite number for animation
 
+    public Rectangle solidArea;  // Rectangle for collision detection
+    public boolean collisionOn = false; // Flag for collision detection
 }
