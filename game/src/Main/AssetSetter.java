7ff42e291114a5ae401d7fa6831b28d57ae0b9f1
mikha <mikhaelmiro300@gmail.com>
2025-05-02 20:28:15 +0200

    feat: update object initialization in AssetSetter to include GamePanel reference

--- game/src/Main/AssetSetter.java
+++ game/src/Main/AssetSetter.java
@@ -16,37 +16,37 @@ public class AssetSetter {
     public void setObject(){
         
         //key
-        gp.obj[0] = new OBJ_Key();
+        gp.obj[0] = new OBJ_Key(gp);
         gp.obj[0].worldX = 23 * gp.tileSize; // 23 tiles from left
         gp.obj[0].worldY = 7 * gp.tileSize; // 7 tiles from top
 
-        gp.obj[1] = new OBJ_Key();
+        gp.obj[1] = new OBJ_Key(gp);
         gp.obj[1].worldX = 23 * gp.tileSize; // 23 tiles from left
         gp.obj[1].worldY = 40 * gp.tileSize; // 7 tiles from top 
 
-        gp.obj[2] = new OBJ_Key();
+        gp.obj[2] = new OBJ_Key(gp);
         gp.obj[2].worldX = 38 * gp.tileSize; 
         gp.obj[2].worldY = 8 * gp.tileSize; 
 
         // door
-        gp.obj[3] = new OBJ_Door();
+        gp.obj[3] = new OBJ_Door(gp);
         gp.obj[3].worldX = 10 * gp.tileSize; 
         gp.obj[3].worldY = 11 * gp.tileSize; 
 
-        gp.obj[4] = new OBJ_Door();
+        gp.obj[4] = new OBJ_Door(gp);
         gp.obj[4].worldX = 8 * gp.tileSize; 
         gp.obj[4].worldY = 28 * gp.tileSize; 
 
-        gp.obj[5] = new OBJ_Door();
+        gp.obj[5] = new OBJ_Door(gp);
         gp.obj[5].worldX = 12 * gp.tileSize; 
         gp.obj[5].worldY = 22 * gp.tileSize; 
 
-        gp.obj[6] = new OBJ_Chest();
+        gp.obj[6] = new OBJ_Chest(gp);
         gp.obj[6].worldX = 10 * gp.tileSize; 
         gp.obj[6].worldY = 7 * gp.tileSize; 
 
         // boots
-        gp.obj[7] = new OBJ_Boots();
+        gp.obj[7] = new OBJ_Boots(gp);
         gp.obj[7].worldX = 37 * gp.tileSize; 
         gp.obj[7].worldY = 42 * gp.tileSize; 
         
